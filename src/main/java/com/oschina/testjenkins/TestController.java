73e0be9cd782128b36da0fefd7c3602aecc7f407
hylyLLL <19519809420@qq.com>
2020-05-25 17:29:22 +0800

    测试

--- src/main/java/com/oschina/testjenkins/TestController.java
+++ src/main/java/com/oschina/testjenkins/TestController.java
@@ -1,4 +1,4 @@
-package com.oschina.testjenkins.test;
+package com.oschina.testjenkins;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
